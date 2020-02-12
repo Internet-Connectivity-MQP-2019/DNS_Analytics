75e8b61289514cfbe055d988d80d1dfd59d6b692
samgoldman <sgoldman216@gmail.com>
2020-02-11 20:50:25 -0500

    Add graph analysis and corresponding CDF functionality

--- dns_analytics.py
+++ dns_analytics.py
@@ -1,3 +1,6 @@
+from functools import total_ordering
+
+import networkx
 import pandas as pd
 import numpy as np
 from scipy.stats import kruskal
@@ -5,7 +8,9 @@ from sklearn.cluster import AgglomerativeClustering
 import argparse
 import os
 import us
-
+from pylab import plot, show
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 def main():
     parser = argparse.ArgumentParser()
@@ -54,8 +59,9 @@ def main():
     # Generating rankings, along with the count for each state
     state_rankings = pairs.groupby(['recursive_state'])[['median']].agg([np.median, 'count'])
     state_rankings.columns = state_rankings.columns.droplevel()
+    state_rankings = state_rankings[state_rankings['count'] >= 250]
     state_rankings = state_rankings.sort_values('median')
-    state_rankings['rank'] = state_rankings['median'].rank(method='max')
+    state_rankings['rank'] = state_rankings['median'].rank(method='max', ascending=(column == 'rtt'))
 
     state_to_state_p_values = pd.DataFrame()
     state_to_state_p_values_i = pd.DataFrame()
@@ -82,6 +88,90 @@ def main():
     state_rankings.to_csv('{}/state_rankings.csv'.format(results_dir))
     state_to_state_p_values.to_csv('{}/state_to_state_p_values.csv'.format(results_dir))
     state_to_state_h_values.to_csv('{}/state_to_state_h_values.csv'.format(results_dir))
+
+    # Graph analysis
+    df = pd.read_csv('{}/state_to_state_p_values.csv'.format(results_dir))
+    df = df.rename(columns={'Unnamed: 0': 'states'})
+    df = df.set_index(['states'])
+    df[df > 0.05] = 1
+    df[df <= 0.05] = 0
+
+    # Create a network from the data
+    net = networkx.from_pandas_adjacency(df)
+
+    # Find cliques and convert to objects
+    raw_cliques = list(networkx.algorithms.community.greedy_modularity_communities(net))
+    cliques = [CliqueOfStates(i, l, [state_rankings.loc[state]['rank'] for state in l],
+                              [state_rankings.loc[state]['median'] for state in l])
+               for i, l in enumerate(raw_cliques)]
+    cliques = sorted(cliques)
+
+    i = 0
+    colors = list(mcolors.TABLEAU_COLORS)
+    # Print cliques and generate intra-clique CDFs
+    for clique in cliques:
+        clique.set_id(i)
+
+        j = 0
+        for state in clique.states:
+            x = pairs.loc[pd.IndexSlice[state], :]['median']
+            color = colors[j % len(colors)]
+            plt.hist(x, bins=400, density=True, cumulative=True, label="{} ({:n})".format(state, clique.ranks[j]),
+                     histtype='step', alpha=0.8, color=color, range=(0, 100))
+            j += 1
+
+        plt.legend(loc='upper left')
+        plt.savefig('{}/cluster_{}_cdf.png'.format(results_dir, i))
+        plt.show()
+
+        i += 1
+        print(clique)
+        print()
+
+    # Generate a CDF for each clique (all cliques)
+    for clique in cliques:
+        x = None
+        for state in clique.states:
+            if x is None:
+                x = pairs.loc[pd.IndexSlice[state], :]['median']
+            else:
+                x = pd.concat([x, pairs.loc[pd.IndexSlice[state], :]['median']])
+
+        color = colors[clique.cid % len(colors)]
+        plt.hist(x, bins=400, density=True, cumulative=True, label="{}".format('%s' % ', '.join(clique.states)),
+                 histtype='step', alpha=0.8, color=color, range=(0, 100))
+        plt.axvline(x=np.median(x), color=color)
+    plt.legend(loc='upper left')
+    plt.savefig('{}/clusters_cdf.png'.format(results_dir))
+    plt.show()
+
+
+@total_ordering
+class CliqueOfStates:
+    def __init__(self, cid, states, ranks, values):
+        self.cid = cid
+        self.states = states
+        self.ranks = ranks
+        self.values = values
+
+    def set_id(self, cid):
+        self.cid = cid
+
+    def mean_value(self):
+        return np.mean(self.values)
+
+    def __str__(self):
+        state_list_string = ""
+        for state, rank, value in sorted(zip(self.states, self.ranks, self.values), key=lambda x: x[1]):
+            state_list_string += "\n\t{}\t{}\t{}".format(state, rank, value)
+
+        return "Clique {}: {}\nMean value: {}".format(self.cid, state_list_string, self.mean_value())
+
+    def __eq__(self, other):
+        return self.mean_value() == other.mean_value()
+
+    def __lt__(self, other):
+        return self.mean_value() < other.mean_value()
 
 
 def get_or_create_pickle(column='rtt', z=2, cov_max=0.1):
